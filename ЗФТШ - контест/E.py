938a1ac578ec38fdd706c4fd7889df654e18610f
Daniel Sazonov <p.294803@gmail.com>
2021-10-25 20:23:28 +0300

    Решил E

--- ЗФТШ - контест/E.py
+++ ЗФТШ - контест/E.py
@@ -1,7 +1,20 @@
+max_1, max_2 = 0, 0
+
 with open('input.txt', 'r') as in_file:
     file = in_file.read().split('\n')
-    n = int(file[0])
-    x = sorted(map(int, file[1].split()))
+    for i in map(int, file[1].split()):
+        if i > max_1:
+            max_1 = i
+        if max_1 > i > max_2:
+            max_2 = i
+#
+# max_1, max_2 = 0, 0
+#
+# for i in x:
+#     if i > max_1:
+#         max_1 = i
+#     if max_1 > i > max_2:
+#         max_2 = i
 
 with open('output.txt', 'w') as out_file:
-    out_file.write(str(x[-1] * x[-2]))
+    out_file.write(str(max_1 * max_2))
