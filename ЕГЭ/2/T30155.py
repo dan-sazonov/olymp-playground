6ed6c51f7d59e874bf3a1a831a21c693c9bb1a2f
Dan Sazonov <p.294803@gmail.com>
2022-03-04 03:53:28 +0300

    Решил T30155

---
+++ ЕГЭ/2/T30155.py
@@ -0,0 +1,21 @@
+print('w', 'x', 'y', 'z')
+
+for w in 0, 1:
+    for x in 0, 1:
+        for y in 0, 1:
+            for z in 0, 1:
+                f = ((x <= y) or (y == w)) and ((x or z) == w)
+                if f:
+                    print(w, x, y, z)
+
+# w x y z
+# 1 0 1 1
+# 1 1 1 0
+# 0 0 1 0
+
+
+# 1 0 0 1
+
+# 1 0 1 1
+# 0 0 1 0
+# zyxw
