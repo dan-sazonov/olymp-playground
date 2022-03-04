e8086b167a6561c6b013bb66b566a3ad68e249f7
Dan Sazonov <p.294803@gmail.com>
2022-03-04 04:01:17 +0300

    Решил T29773

---
+++ ЕГЭ/2/T29773.py
@@ -0,0 +1,14 @@
+print('w', 'x', 'y', 'z')
+
+for w in 0, 1:
+    for x in 0, 1:
+        for y in 0, 1:
+            for z in 0, 1:
+                f = ((y <= z) or (not x and w)) == (w == z)
+                if f:
+                    print(w, x, y, z)
+# w x y z
+# 1 0 0 1
+# 1 0 1 1
+#
+# 1 1 1 0
