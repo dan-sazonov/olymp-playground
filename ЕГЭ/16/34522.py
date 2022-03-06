3f82842af78e90a796f9db4c3db895135110108d
Dan Sazonov <p.294803@gmail.com>
2022-03-06 04:20:24 +0300

    Решил 34522

---
+++ ЕГЭ/16/34522.py
@@ -0,0 +1,10 @@
+# x&51 = 0 ∨ (x&41 = 0 → x&А = 0)
+
+a = 0
+while True:
+    for x in range(1_000_000):
+        if not ((x & 51 == 0) or ((x & 41 == 0) <= (x & a == 0))):
+            break
+    else:
+        print(a)
+    a += 1
