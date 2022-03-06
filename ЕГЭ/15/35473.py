a2cafafe8e085c6587cf9c327768004ea4a21e5b
Dan Sazonov <p.294803@gmail.com>
2022-03-06 03:27:24 +0300

    Переделал 35473

--- ЕГЭ/15/35473.py
+++ ЕГЭ/15/35473.py
@@ -7,10 +7,9 @@ def f(n, m):
 
 a = 1
 while True:
-    for x in range(1, 1_000_000):
-        if not(f(a, 45) and (f(750, x))) <= ((not f(a, x)) <= (not f(120, x))):
+    for x in range(1, 100_000):
+        if not (f(a, 45) and (f(750, x) <= ((not f(a,x)) <= (not f(120, x))))):
             break
     else:
-        print(f'=========={a}')
-    print(a)
+        print(a)
     a += 1
