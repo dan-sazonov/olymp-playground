e2f29bde35e29388354f66dbadd5dfa806784396
Dan Sazonov <p.294803@gmail.com>
2022-04-24 20:33:14 +0300

    Решил 33494

---
+++ ЕГЭ/24/33494.py
@@ -0,0 +1,13 @@
+with open('8.txt') as f:
+    f = f.read()
+
+c = dict()
+
+for i in range(len(f) - 1):
+    if f[i] == 'E':
+        if f[i + 1] in c.keys():
+            c[f[i + 1]] += 1
+        else:
+            c[f[i + 1]] = 1
+
+print(sorted(c.items(), key=lambda x: x[1], reverse=True))
