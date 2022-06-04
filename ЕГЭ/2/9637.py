85731d019843e547f9b4b60c86726f05c3721991
Dan Sazonov <p.294803@gmail.com>
2022-06-05 02:13:10 +0300

    Решил 9637

---
+++ ЕГЭ/2/9637.py
@@ -0,0 +1,17 @@
+# (¬z)∧x.
+
+print('x z f')
+
+for x in 0, 1:
+    for z in 0, 1:
+        print(x, z, int(not z and x))
+
+# z     x
+# 0	0	0	0
+# 0	0	1	1
+# 0	1	0	0
+# 0	1	1	1
+# 1	0	0	0
+# 1	0	1	0
+# 1	1	0	0
+# 1	1	1	0
