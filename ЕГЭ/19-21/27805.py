6b6be19f469da2a5715f18d3461adad905ecff34
Dan Sazonov <p.294803@gmail.com>
2022-06-17 01:53:43 +0300

    Решил 27807

--- ЕГЭ/19-21/27805.py
+++ ЕГЭ/19-21/27805.py
@@ -10,6 +10,11 @@ def win2(s):
     return loss1(s + 1) or loss1(s + 4) or loss1(s * 5)
 
 
+def loss12(s):
+    return (win1(s + 1) or win2(s + 1)) and (win1(s + 4) or win2(s + 4)) and (win1(s * 5) or win2(s * 5))
+# fucked up
+
+
 for s in range(1, 63):
-    if win2(s):
+    if loss12(s):
         print(s)
