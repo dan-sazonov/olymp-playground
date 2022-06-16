bd95a737424d2662c01d66d2edb0f5bc5dc84cbd
Dan Sazonov <p.294803@gmail.com>
2022-06-16 15:55:08 +0300

    Решил 27806

--- ЕГЭ/19-21/27805.py
+++ ЕГЭ/19-21/27805.py
@@ -2,6 +2,14 @@ def win1(s):
     return (s + 1 >= 63) or (s + 4 >= 63) or (s * 5 >= 63)
 
 
+def loss1(s):
+    return not (win1(s)) and win1(s + 1) and win1(s + 4) and win1(s * 5)
+
+
+def win2(s):
+    return loss1(s + 1) or loss1(s + 4) or loss1(s * 5)
+
+
 for s in range(1, 63):
-    if win1(s + 1) or win1(s + 4) or win1(s * 5):
+    if win2(s):
         print(s)
