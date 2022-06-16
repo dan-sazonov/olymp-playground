459dae4e134a197ecd80f034e6946d2989a79ee0
Dan Sazonov <p.294803@gmail.com>
2022-06-16 16:05:31 +0300

    Решил 27963

--- ЕГЭ/19-21/27962.py
+++ ЕГЭ/19-21/27962.py
@@ -2,6 +2,14 @@ def win1(s):
     return (s + 1 >= 48) or (s + 3 >= 48) or (s * 2 >= 48)
 
 
+def loss1(s):
+    return not win1(s) and win1(s + 1) and win1(s + 3) and win1(s * 2)
+
+
+def win2(s):
+    return loss1(s + 1) or loss1(s + 3) or loss1(s * 2)
+
+
 for s in range(1, 48):
-    if win1(s + 1) or win1(s + 3) or win1(s * 2):
+    if win2(s):
         print(s)
