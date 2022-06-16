8dd78bd3eddca8777c65ff0cc729ea0d089c1bf5
Dan Sazonov <p.294803@gmail.com>
2022-06-16 16:10:53 +0300

    Решил 28152

--- ЕГЭ/19-21/28151.py
+++ ЕГЭ/19-21/28151.py
@@ -2,6 +2,14 @@ def win1(s):
     return (s + 1 >= 30) or (s * 3 >= 30)
 
 
+def loss1(s):
+    return not win1(s) and win1(s + 1) and win1(s * 3)
+
+
+def win2(s):
+    return loss1(s + 1) or loss1(s * 3)
+
+
 for s in range(1, 30):
-    if win1(s + 1) or win1(s * 3):
+    if win2(s):
         print(s)
