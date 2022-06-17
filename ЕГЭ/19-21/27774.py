c842743faf5ecef231a64ebd9148d815d4a2869f
Dan Sazonov <p.294803@gmail.com>
2022-06-17 06:48:03 +0300

    Решил 27774-27776

---
+++ ЕГЭ/19-21/27774.py
@@ -0,0 +1,27 @@
+import math
+
+
+def win1(x, s):
+    return (x - 1 + s <= 20) or (x + s - 1 <= 20) or (math.ceil(x / 2) + s <= 20) or (math.ceil(s / 2) + x <= 20)
+
+
+def loss1(x, s):
+    return (not win1(x, s)) and win1(x - 1, s) and win1(x, s - 1) and win1(math.ceil(x / 2), s) and win1(x, math.ceil(
+        s / 2))
+
+
+def win2(x, s):
+    return loss1(x - 1, s) or loss1(x, s - 1) or loss1(math.ceil(x / 2), s) or loss1(x, math.ceil(s / 2))
+
+
+def loss12(x, s):
+    return ((win1(x - 1, s) or win2(x - 1, s)) and (win1(x, s - 1) or win2(x, s - 1)) and
+            (win1(math.ceil(x / 2), s) or win2(math.ceil(x / 2), s)) and (
+                win1(x, math.ceil(s / 2) or win2(x, math.ceil(s / 2))))
+            and (win2(x - 1, s) or win2(x, s - 1) or win2(math.ceil(x / 2), s) or win2(x, math.ceil(s / 2))))
+
+
+x = 10
+for s in range(11, 150):
+    if loss12(x, s):
+        print(s)
