4ee38b962b0805606a9a4b127855987180dd24ec
Dan Sazonov <p.294803@gmail.com>
2022-06-17 05:57:42 +0300

    Решил 33191-33193

---
+++ ЕГЭ/19-21/33191.py
@@ -0,0 +1,21 @@
+def win1(x, s):
+    return (x + 1 + s >= 91) or (x * 4 + s >= 91) or (x + s * 4 >= 91)
+
+
+def loss1(x, s):
+    return (not win1(x, s)) and win1(x, s + 1) and win1(x, s * 4) and win1(x + 1, s) and win1(x * 4, s)
+
+
+def win2(x, s):
+    return loss1(x, s + 1) or loss1(x, s * 4) or loss1(x + 1, s) or loss1(x * 4, s)
+
+
+def loss12(x, s):
+    return ((win1(x, s + 1) or win2(x, s + 1)) and (win1(x, s * 4) or win2(x, s * 4)) and
+            (win1(x + 1, s) or win2(x + 1, s)) and (win1(x * 4, s) or win2(x * 4, s)))
+
+
+x = 5
+for s in range(1, 86):
+    if loss12(x, s):
+        print(s)
