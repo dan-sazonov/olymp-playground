4925f137a928df248c067508bbe2671175b81459
Dan Sazonov <p.294803@gmail.com>
2022-06-19 10:31:04 +0300

    Решил P265

---
+++ ЕГЭ/17/P265.py
@@ -0,0 +1,15 @@
+arr = list(map(int, open('17-243.txt')))
+amn = 0
+c = 0
+m = -20001
+
+for j in arr:
+    if j % 51 == 0:
+        amn += sum(map(int, str(j)))
+
+for i in range(len(arr) - 1):
+    if (arr[i] < amn) or (arr[i+1] < amn):
+        c += 1
+        m = max(m, arr[i] + arr[i+1])
+
+print(c, m)
