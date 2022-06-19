535e79d141f73a6c5b7e0f85526f03eca8969e9d
Dan Sazonov <p.294803@gmail.com>
2022-06-19 09:29:12 +0300

    Решил 37355

--- ЕГЭ/17/37355.py
+++ ЕГЭ/17/37355.py
@@ -3,12 +3,9 @@ c = 0
 m = 0
 
 for i in range(len(arr) - 1):
-    for j in range(i, len(arr) - 1):
-        if (arr[i] + arr[i+1]) % 7 == 0:
+    for j in range(i + 1, len(arr) - 1):
+        if (arr[i] + arr[j]) % 7 == 0:
             c += 1
-            m = max(m, (arr[i] + arr[i+1]))
-
-c += 1
-m = max(m, (arr[i] + arr[i+1]))
+            m = max(m, (arr[i] + arr[j]))
 
 print(c, m)
